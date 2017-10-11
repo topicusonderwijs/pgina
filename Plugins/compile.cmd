bd9aca3665e28f631411c1ace187384c26b794e1
Bas Magré <Opvolger@gmail.com>
2017-10-11 17:11:03 +0200

    remove nuget from plugins

--- Plugins/compile.cmd
+++ Plugins/compile.cmd
@@ -6,12 +6,6 @@ if "%VCpath%"=="" call %~dps0..\getVCpath.cmd || exit /b 1
 for /f "tokens=*" %%e in ('dir %~dps0*.sln /B /S /A') do (
 	@echo.
 	@echo compiling %%~ne
-    
-    IF EXIST "%%~dpe\packages.config" (
-        echo "getting nuget packages for %%~dpe"
-        %~dps0..\.nuget\NuGet.exe install "%%~dpe\packages.config" -OutputDirectory "%%~dpe\packages"
-        @echo.
-    )
 	%VCpath% %%~se /Rebuild "Release|Any CPU" /Out %~dps0%%~ne.log || (
 		@echo failed to compile
 		pause
